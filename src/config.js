a8f2929c645d149bf304fbba53a93664d210dd41
Stephanie <stephaniejoychamblee@gmail.com>
2018-06-10 07:32:49 -0400

    Update API enpoint

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 
 export const API_BASE_URL =
-process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+process.env.REACT_APP_API_BASE_URL || 'https://fierce-hollows-76641.herokuapp.com/api';
